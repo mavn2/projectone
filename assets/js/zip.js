a2c30658f9466b77005e66bc0fc5fb9e21815680
pratyu <pratyusha.raghupatruni@gmail.com>
2020-07-27 21:58:09 -0700

    distance added but not on html

--- assets/js/zip.js
+++ assets/js/zip.js
@@ -1,4 +1,8 @@
+var i=0;
+
+
 $(document).ready(function () {
+
     $("#homepage").show();
     $("#results-content").hide();
     $("#breweries-list").hide();
@@ -15,6 +19,7 @@ $("#submit").on("click", function (event) {
         return false;
     }
     breweriesNearby(zipCode);
+    DistanceCalc(zipCode);
     $("#zip-code").val("");
 });
 
@@ -25,11 +30,11 @@ $("#homeIcon").on("click", function () {
 });
 
 function breweriesNearby(zipCode) {
- 
+    console.log("test : " + zipCode);
 
     //API URL for fetching the temperature
     var settings = {
-        "url": "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=brewery+in+redmond&key=AIzaSyCwva93H8v_VpIqPiZ75_0hm0eoKqw4Dgw",
+        "url": "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=brewery+in+" + zipCode + "&key=AIzaSyCwva93H8v_VpIqPiZ75_0hm0eoKqw4Dgw",
         "method": "GET",
         "timeout": 0,
         "headers": {
@@ -38,32 +43,79 @@ function breweriesNearby(zipCode) {
     };
     $.ajax(settings).done(function (response) {
         console.log(response);
-          $("#results-content").empty();
-        var resultsSearch=response.results.length-10
-        
-            for (i = 0; i < resultsSearch ; i++) {
-                var breweryDiv=$("<div class='results'>");
-                breweryDiv.attr("id","results"+[i]);
 
-                var breweryImage = $("<img class='brewerylogo'>").attr("src", "./assets/images/sampleimage.jpg")
+        $("#results-content").empty();
+    
+        var resultsSearch = response.results.length;
+
+        for (i = 0; i < resultsSearch; i++) {
+            if (i < 10) {
+                var breweryDiv = $("<div class='results'>");
+                breweryDiv.attr("id", "results" + [i]);
+
+                var breweryImage = $("<img class='brewerylogo'>").attr("src", "./assets/images/sampleimage.jpg");
                 breweryDiv.append(breweryImage);
 
                 var breweryName = $("<p class='title'>").text(response.results[i].name);
                 breweryDiv.append(breweryName);
-    
+
                 var businessStatus = $("<p>").text("Business Status : " + response.results[i].business_status);
                 breweryDiv.append(businessStatus);
-                console.log(response.results[i].business_status);
-    
+
                 var breweryRating = $("<p>").text("Rating: " + response.results[i].rating);
                 breweryDiv.append(breweryRating);
-                console.log(response.results[i].rating);
-    
-                var breweryDistance = $("<p>").text("distance");
-                breweryDiv.append(breweryDistance);
+
+
+                var breweryPlaceid = response.results[i].place_id;
+
+                DistanceCalc(breweryPlaceid, zipCode);
 
                 $("#results-content").append(breweryDiv);
+
             }
+        }
+    });
+
+}
+
+function DistanceCalc(breweryPlaceid, zipCode) {
+
+    var settings = {
+        "url": "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + zipCode + "&key=AIzaSyCwva93H8v_VpIqPiZ75_0hm0eoKqw4Dgw",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Authorization": "Bearer 6HRV34H2QG8huioiDfP9_Fznc-Ig3gAaHcyGdAzjFquxiOqekhtCetxAkYZXWu-lV5UuqlnR5VWE2D0j8yzqs458Su9bnnRuU0XlrFUxjVymamjNuDVOdJtImV4aX3Yx"
+        },
+    };
+    $.ajax(settings).done(function (originresponse) {
+        var placeId = originresponse.results[0].place_id;
+
+        var settings = {
+            "url": "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=place_id:" + placeId + "&destinations=place_id:" + breweryPlaceid + "&key=AIzaSyCwva93H8v_VpIqPiZ75_0hm0eoKqw4Dgw",
+            "method": "GET",
+            "timeout": 0,
+        };
+
+        $.ajax(settings).done(function (distanceresponse) {
+            console.log(distanceresponse);
+             
+            var distance = distanceresponse.rows[0].elements[0].distance.text;
+            console.log(distance);
+            var duration = distanceresponse.rows[0].elements[0].duration.text;
+            console.log(duration);
+    
+            var breweryDistance = $("<p>").text("distance :" + distance);
+            $("#"+"results"+[i]).append(breweryDistance);
+            var travelDuration = $("<p>").text("duration : " + duration);
+            $("#"+"results"+[i]).append(travelDuration);
+             
         });
-        
+
+    });
+
+
+
+
+
 }
